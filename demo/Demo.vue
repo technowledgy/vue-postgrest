7f4402729af0a34361a5ac738010e0494e6795b4
Julian Walther <julian.walther@technowledgy.de>
2020-02-04 12:04:25 +0100

    feat: add demo

---
+++ demo/Demo.vue
@@ -0,0 +1,11 @@
+<template>
+  <postgrest
+    route="clients">
+  </postgrest>
+</template>
+
+<script>
+export default {
+  name: 'Demo'
+}
+</script>
--- demo/main.js
+++ demo/main.js
@@ -1,9 +1,14 @@
 // file only for dev. purposes
 import Vue from 'vue'
-import VuePostgrest from '@/Postgrest'
+import Postgrest from '@/index'
+import Demo from './Demo'
 
 Vue.config.productionTip = false
 
+Vue.use(Postgrest, {
+  apiRoot: 'api/'
+})
+
 new Vue({
-  render: h => h(VuePostgrest)
+  render: h => h(Demo)
 }).$mount('#app')
