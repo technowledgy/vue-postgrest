f7e73d377e01a225081246cc8d3bb6505fcfc441
Julian Walther <julian.walther@technowledgy.de>
2020-02-11 14:24:55 +0100

    tests(mockApi): add resp. headers and range

--- tests/unit/Postgrest/MockApi.config.js
+++ tests/unit/Postgrest/MockApi.config.js
@@ -5,7 +5,20 @@ module.exports = function (mockData) {
       fixtures: function (match, params, headers, context) {
         for (const endpoint in mockData.get) {
           if (match[1] === endpoint) {
-            return headers['Accept'] === 'application/vnd.pgrst.object+json' ? mockData.get[endpoint][0] : mockData.get[endpoint]
+            const resp = {
+              body: [],
+              headers: {}
+            }
+            resp.body = headers['Accept'] === 'application/vnd.pgrst.object+json' ? mockData.get[endpoint][0] : mockData.get[endpoint]
+            if (headers.Range && Array.isArray(resp.body)) {
+              resp.headers['Range-Unit'] = 'items'
+              // setting content range properly to actual returned range not neccessary for the simple test cases
+              const range = headers.Range.split('-')
+              const retRange = range[1] ? range.join('-') : [range[0], resp.body.length].join('-')
+              resp.headers['Content-Range'] = headers.Prefer === 'count=exact' ? retRange + '/' + resp.body.length : retRange + '/*'
+              resp.body = resp.body.slice(...range)
+            }
+            return resp
           }
         }
       },
