67dd22fa4ad9d59a46e34d558f91936aef805443
Julian Walther <julian.walther@technowledgy.de>
2020-02-12 15:16:30 +0100

    feat(SchemaManager): only cache the primary keys, not the whole schema

--- src/SchemaManager.js
+++ src/SchemaManager.js
@@ -1,7 +1,6 @@
 import superagent from 'superagent'
 
 export default {
-  // TODO: do not cache the whole schema
   cache: {},
   resetCache () {
     this.cache = {}
@@ -10,20 +9,21 @@ export default {
     if (!this.cache[apiRoot]) {
       await this._getSchema(apiRoot) 
     }
-    let keys = {}
-    if (this.cache[apiRoot] && this.cache[apiRoot].definitions) {
-      Object.keys(this.cache[apiRoot].definitions).map(table => {
-        keys[table] = this.cache[apiRoot].definitions[table].required 
-      })
-    }
-    return keys
+    return this.cache[apiRoot]
   },
   async _getSchema (apiRoot) {
     const resp = await superagent.get(apiRoot) 
-    if (resp && resp.headers['content-type'] === 'application/openapi+json') {
-      this.cache[apiRoot] = resp.body
+    if (resp && resp.headers['content-type'] === 'application/openapi+json' && resp.body) {
+      this.cache[apiRoot] = resp.body.definitions ? this._extractPrimaryKeys(resp.body.definitions) : {}
     } else {
       // TODO: emit not an api error
     }
+  },
+  _extractPrimaryKeys (def) {
+    let keys = {}
+    Object.keys(def).map(table => {
+      keys[table] = def[table].required 
+    })
+    return keys
   }
 }
