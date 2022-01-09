fe64bf373b6ee6a43a0077a3d8c036623b8c84c9
Wolfgang Walther <walther@technowledgy.de>
2022-01-09 11:41:33 +0100

    chore: Update development deps

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,37 @@
+module.exports = { 
+  base: '/vue-postgrest/',
+  title: 'vue-postgrest',
+  description: 'PostgREST integration for Vue.js',
+  host: 'localhost',
+  themeConfig: {
+    repo: 'technowledgy/vue-postgrest',
+    docsDir: 'docs',
+    editLinks: true,
+    smoothScroll: true,
+    sidebarDepth: 2,
+    sidebar: [ 
+      '/start/',
+      '/guide/',
+      '/api/',
+      '/query/'
+    ],
+    lastUpdated: 'Last Updated',
+    nextLinks: false,
+    prevLinks: false
+  },
+  plugins: [ 
+    '@vuepress/active-header-links',
+    '@vuepress/back-to-top'
+  ],
+  chainWebpack: (config) => {
+    config.module
+      .rule('js')
+      .use('babel-loader')
+      .tap(options => Object.assign(options, {
+        plugins: [
+          '@babel/plugin-proposal-optional-chaining',
+          '@babel/plugin-proposal-nullish-coalescing-operator'
+        ]
+      }))
+    }
+}
