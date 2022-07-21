29e643d4ed7c947e1283ef4794d51cbbc39249db
Wolfgang Walther <walther@technowledgy.de>
2022-07-21 16:37:25 +0200

    chore: Switch from vite-plugin-vue2 to @vitejs/plugin-vue2
    Signed-off-by: Wolfgang Walther <walther@technowledgy.de>

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { fileURLToPath } from 'url'
 import { dirname, resolve } from 'path'
 import { defineConfig } from 'vite'
-import { createVuePlugin } from 'vite-plugin-vue2'
+import vue from '@vitejs/plugin-vue2'
 import copy from 'rollup-plugin-copy'
 
 const repoDir = dirname(fileURLToPath(import.meta.url))
@@ -23,7 +23,7 @@ export default defineConfig({
     sourcemap: true
   },
   plugins: [
-    createVuePlugin(),
+    vue(),
     copy({
       targets: [
         { src: 'package.json.cjs', dest: 'dist/cjs', rename: 'package.json' }
