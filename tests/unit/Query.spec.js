technowledgy/vue-postgrest

commit 030e2dd052c7fcc42a9436fcc01680a10c54142e

fix: Only quote in. operator, but not regular strings in operators or arguments

src/Query.js
@@ -156,7 +156,7 @@ class Query extends URL {
         let strValue
         switch (ops[ops.length - 1]) {
           case 'in':
-            strValue = this._valueToString(value, '()')
+            strValue = this._valueToString(value, '()', true)
             break
           case undefined:
             // no operator + object = nested json
@@ -176,19 +176,19 @@ class Query extends URL {
     }).flat().filter(Boolean)
   }
 
-  _valueToString (value, arrayBrackets = '{}') {
+  _valueToString (value, arrayBrackets = '{}', quoteStrings = false) {
     if (value === null) {
       return 'null'
     } else if (typeof value === 'boolean') {
       return value.toString()
     } else if (Array.isArray(value)) {
-      return arrayBrackets.charAt(0) + value.map(v => this._valueToString(v)).join(',') + arrayBrackets.charAt(1)
+      return arrayBrackets.charAt(0) + value.map(v => this._valueToString(v, '{}', quoteStrings)).join(',') + arrayBrackets.charAt(1)
     } else if (typeof value === 'object') {
       // range type
       const { lower, includeLower = true, upper, includeUpper = false } = value
       return (includeLower ? '[' : '(') + lower + ',' + upper + (includeUpper ? ']' : ')')
     } else {
-      return quoteValue(value)
+      return quoteStrings ? quoteValue(value) : value
     }
   }
 }

tests/unit/Query.spec.js
@@ -37,29 +37,27 @@ describe('Query', () => {
 
     itt('represents integer as string', { int: 1 }, 'int=1')
 
-    itt('quotes float', { pi: 3.14 }, 'pi="3.14"')
+    itt('does not quote float', { pi: 3.14 }, 'pi=3.14')
 
-    itt('quotes values with reserved postgrest characters', { str: 'test.test' }, 'str="test.test"')
+    itt('does not quote values with reserved postgrest characters', { str: 'test.test' }, 'str=test.test')
 
     itt('represents null as string', { null: null }, 'null=null')
 
-    itt('quotes string null', { str: 'null' }, 'str="null"')
+    itt('does not quote string null', { str: 'null' }, 'str=null')
 
     itt('represents true as string', { bool: true }, 'bool=true')
 
-    itt('quotes string true', { str: 'true' }, 'str="true"')
+    itt('does not quote string true', { str: 'true' }, 'str=true')
 
     itt('represents false as string', { bool: false }, 'bool=false')
 
-    itt('quotes string false', { str: 'false' }, 'str="false"')
+    itt('does not quote string false', { str: 'false' }, 'str=false')
 
     itt('supports arguments with array values', { arr: [1, 2, 3] }, 'arr={1,2,3}')
   })
 
   describe('horizontal filtering', () => {
     itt('sets single condition', { 'id.eq': 1 }, 'id=eq.1')
-
-    itt('quotes only value and not operator', { 'pi.eq': 3.14 }, 'pi=eq."3.14"')
 
     itt('sets multiple conditions', {
       'id.eq': 1,
@@ -122,6 +120,8 @@ describe('Query', () => {
     itt('supports full-text search operator options in key', { 'my_tsv.fts(french)': 'amusant' }, 'my_tsv=fts(french).amusant')
 
     itt('supports "in" operator with array', { 'id.in': [1, 2, 3] }, 'id=in.(1,2,3)')
+
+    itt('quotes values with reserved postgrest characters in "in" operator', { 'str.in': ['a.b', 'x', 'c,d', 5, 'e:f', true, '(', ')'] }, 'str=in.("a.b",x,"c,d",5,"e:f",true,"(",")")')
 
     itt('supports other operators with arrays', { 'tags.cs': ['example', 'new'] }, 'tags=cs.{example,new}')
 

commit f9a5183c10026a44dc7f26d7aa4ded475e5cbb37

fix: Quote values inside logical operators
This is a regression introduced in 030e2dd0 / 1.4.1.

src/Query.js
@@ -134,7 +134,7 @@ class Query extends URL {
     }
   }
 
-  _parseConditions (obj, jsonPrefix = '') {
+  _parseConditions (obj, jsonPrefix = '', quoteStrings = false) {
     return Object.entries(obj).map(([key, value]) => {
       if (value === undefined) return false
       // throw away alias - just used to allow the same condition more than once on one object
@@ -143,7 +143,7 @@ class Query extends URL {
       if (isLogicalOperator(key)) {
         if (!value || typeof value !== 'object' || Array.isArray(value)) throw new Error('no object for logical operator')
         if (jsonPrefix) throw new Error('logical operators can\'t be nested with json operators')
-        const strValue = this._parseConditions(value).map(({ key: k, value: v }) => {
+        const strValue = this._parseConditions(value, '', true).map(({ key: k, value: v }) => {
           return isLogicalOperator(k) ? `${k}${v}` : `${k}.${v}`
         }).join(',')
         if (!strValue) return undefined
@@ -161,11 +161,11 @@ class Query extends URL {
           case undefined:
             // no operator + object = nested json
             if (value && typeof value === 'object' && !Array.isArray(value)) {
-              return this._parseConditions(value, cc('', jsonPrefix, '->') + field)
+              return this._parseConditions(value, cc('', jsonPrefix, '->', quoteStrings) + field)
             }
             // falls through
           default:
-            strValue = this._valueToString(value)
+            strValue = this._valueToString(value, '{}', quoteStrings)
         }
         const jsonOperator = typeof value === 'string' ? '->>' : '->'
         return {
@@ -176,7 +176,7 @@ class Query extends URL {
     }).flat().filter(Boolean)
   }
 
-  _valueToString (value, arrayBrackets = '{}', quoteStrings = false) {
+  _valueToString (value, arrayBrackets, quoteStrings) {
     if (value === null) {
       return 'null'
     } else if (typeof value === 'boolean') {

tests/unit/Query.spec.js
@@ -83,6 +83,15 @@ describe('Query', () => {
       }
     }, 'or=(id.eq.1,name.eq.test)')
 
+    itt('quotes values with reserved postgrest characters in "or" object', {
+      or: {
+        'id.gt': 1,
+        'salutation.eq': 'Hello, World!',
+        'simple.eq': 'stuff',
+        'name.eq': 'special: (case)'
+      }
+    }, 'or=(id.gt.1,salutation.eq."Hello,+World!",simple.eq.stuff,name.eq."special:+(case)")')
+
     itt('supports logical disjoining with aliased keys', {
       or: {
         'f:nb.is': false,