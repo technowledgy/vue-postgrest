904c8bd8a60adb3e742b2d7a8e9250f342566a70
Christian Friedow <christian@friedow.com>
2026-04-27 13:33:43 +0200

    feat: expose setDefaultHeaders in the public library interface

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import Plugin from '@/Plugin'
 import { resetSchemaCache, setDefaultToken } from '@/Schema'
+import { setDefaultHeaders } from '@/request'
 import pg from '@/mixin'
 import { AuthError, FetchError, PrimaryKeyError, SchemaNotFoundError } from '@/errors'
 import usePostgrest from '@/use'
 
-export { pg, AuthError, FetchError, PrimaryKeyError, SchemaNotFoundError, resetSchemaCache, setDefaultToken, usePostgrest, Plugin as default }
+export { pg, AuthError, FetchError, PrimaryKeyError, SchemaNotFoundError, resetSchemaCache, setDefaultHeaders, setDefaultToken, usePostgrest, Plugin as default }
